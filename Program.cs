1569e56b6b80c6e9937593da6bf3f339ba4e4c39
Luiz Henrique <lmenezesmessias@gmail.com>
2021-03-06 15:27:04 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exerc_cio_Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pergunta = Console.ReadLine();
+
+            Console.WriteLine(pergunta.ToUpper());
+    
+
+        }
+    }
+}
